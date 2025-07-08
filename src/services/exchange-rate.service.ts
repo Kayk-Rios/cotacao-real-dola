b2962230081ba44e2846ecbac5225436bcef09f5
Kayk_Darios <manjac688@gmail.com>
2025-07-08 16:41:55 -0300

    feat: implementar ExchangeRateService com métodos de taxa atual e diária

--- src/services/exchange-rate.service.ts
+++ src/services/exchange-rate.service.ts
@@ -1,4 +1,6 @@
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export interface CurrentExchangeRate {
   success: boolean;
@@ -24,4 +26,36 @@ export interface DailyExchangeResponse {
   to: string;
   data: DailyExchangeData[];
   rateLimitExceeded?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExchangeRateService {
+  private readonly BASE_URL = 'https://api-brl-exchange.actionlabs.com.br/api/1.0/open';
+  private readonly API_KEY = 'RVZG0GHEV2KORLNA';
+
+  constructor(private http: HttpClient) { }
+
+  getCurrentExchangeRate(simboloOrigem: string, simboloDestino: string = 'BRL'): Observable<CurrentExchangeRate> {
+    const url = `${this.BASE_URL}/currentExchangeRate`;
+    const parametros = {
+      apiKey: this.API_KEY,
+      from_symbol: simboloOrigem,
+      to_symbol: simboloDestino
+    };
+
+    return this.http.get<CurrentExchangeRate>(url, { params: parametros });
+  }
+
+  getDailyExchangeRate(simboloOrigem: string, simboloDestino: string = 'BRL'): Observable<DailyExchangeResponse> {
+    const url = `${this.BASE_URL}/dailyExchangeRate`;
+    const parametros = {
+      apiKey: this.API_KEY,
+      from_symbol: simboloOrigem,
+      to_symbol: simboloDestino
+    };
+
+    return this.http.get<DailyExchangeResponse>(url, { params: parametros });
+  }
 }
